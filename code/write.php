c74bf677649ad241dccc578c2da3bfd2c04c9070
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-09 23:28:57 +0100

    In write.php make sure that $_POST['text'] is not empty, else don't write.

--- code/write.php
+++ code/write.php
@@ -4,7 +4,8 @@ $text = $_POST['text'];
 # Undo damage that results from PHP's magical_quotes horrors.
 if (get_magic_quotes_gpc()) $text = stripslashes($text);
 
-file_put_contents($page_path, $text);
+# Only write to file if a non-empty $_POST['text'] got delivered.
+if ($text) file_put_contents($page_path, $text);
 
 echo '<head>
 <meta http-equiv="refresh" content="0; URL=plomwiki.php?title='.$title.'" />
