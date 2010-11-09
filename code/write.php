92385ee95acc4e63c56fe2b574806b14f3191ccd
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-10 00:27:11 +0100

    Minor comment change.

--- code/write.php
+++ code/write.php
@@ -4,7 +4,7 @@ $text = $_POST['text'];
 # Undo damage that results from PHP's magical_quotes horrors.
 if (get_magic_quotes_gpc()) $text = stripslashes($text);
 
-# Don't write any empty $text. Page eradication happens for $text == 'delete'.
+# Don't write any empty $text. Do page eradication only for $text == 'delete'.
 if ($text) 
 { file_put_contents($page_path, $text);
   if ($text == 'delete') unlink($page_path); }
