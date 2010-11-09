ab1b8ff9d4800a666184b549f2ad37fd5676508e
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-09 20:33:55 +0100

    In code/write.php, undo PHP magical_quotes horrors with stripslashes().

--- code/write.php
+++ code/write.php
@@ -1,5 +1,10 @@
 <?php
-file_put_contents($page_path, $_POST['text']);
+$text = $_POST['text'];
+
+# Undo damage that results from PHP's magical_quotes horrors.
+if (get_magic_quotes_gpc()) $text = stripslashes($text);
+
+file_put_contents($page_path, $text);
 
 echo '<head>
 <meta http-equiv="refresh" content="0; URL=plomwiki.php?title='.$title.'" />
