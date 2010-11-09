29aba5bf8a0aeea8ad07010495d8f83a5e6dc8d4
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-09 22:18:33 +0100

    Added line-break and paragraph markup to view.php.

--- code/view.php
+++ code/view.php
@@ -5,9 +5,14 @@ echo '<body>
 if (is_file($page_path)) $text = file_get_contents($page_path); 
 else $text = 'Page does not exist.';
 
-$text = str_replace('<', '&lt;', $text);
+# Change < and > to their HTML entities to avoid user-generated HTML tags.
+$text = str_replace('<', '&lt;', $text); 
 $text = str_replace('>', '&gt;', $text);
 
-echo "\n".$text."\n";
+# Line-break and paragraph markup.
+$text = str_replace("\r\n\r", "\n".'</p>'."\n".'<p>', $text);
+$text = str_replace("\r", '<br />', $text);
+
+echo "\n".'<p>'."\n".$text."\n".'</p>'."\n";
 
 echo '</body>';
