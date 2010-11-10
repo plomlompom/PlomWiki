5343f7a3753a5eb21625e52e0e0cbaf308aba3fc
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-10 14:40:10 +0100

    Rewrote how the code for the user-chosen action is added in plomwiki.php. Now allows for any user action "action=" that has a file ending with ".php" in the code/ directory.

--- plomwiki.php
+++ plomwiki.php
@@ -4,21 +4,25 @@
 $title = $_GET['title']; 
 if (!preg_match('/^[a-zA-Z0-9]+$/', $title)) { echo 'Bad page title'; exit(); }
 
+# Where page data is located.
 $data_dir  = 'pages/';
 $page_path = $data_dir.$title;
 
+# Find appropriate code for user's '?action='. Assume "view.php" if not found.
+$code_dir  = 'code/';
+$fallback = $code_dir.'view.php';
+$action = $_GET['action'];
+$action_path = ($code_dir.$action.'.php'); 
+if (!is_file($action_path)) $action_path = $fallback;
+
+# Final HTML.
 echo '<!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 ';
-
-$code_dir  = 'code/';
-$action = $_GET['action'];
-if     ($action == 'edit')  require($code_dir.'edit.php'); 
-elseif ($action == 'write') require($code_dir.'write.php');
-else                        require($code_dir.'view.php');
-
+require($action_path);
 echo '
 </body>
 </html>';
+
