b5956a729990fb63f653b1291e20f5a2fdb0575d
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-10 05:30:33 +0100

    Re-structured the HTML building in view.php

--- code/view.php
+++ code/view.php
@@ -1,10 +1,4 @@
 <?php
-echo '<title>'.$title.'</title>
-</head>
-<body>
-<p>'.$title.': <a href="plomwiki.php?title='.$title.
-                                               '&amp;action=edit">Edit</a></p>';
-
 if (is_file($page_path)) 
 { $text = file_get_contents($page_path); 
 
@@ -26,7 +20,11 @@ else $text = 'Page does not exist. <a href="plomwiki.php?title='.$title.
                                                 '&amp;action=edit">Create?</a>';
 
 # Final HTML.
-echo '
+echo '<title>'.$title.'</title>
+</head>
+<body>
+<p>'.$title.': <a href="plomwiki.php?title='.$title.
+                                               '&amp;action=edit">Edit</a></p>
 <p>
 '.$text.'
 </p>';
