10ffd1828a1cdcf72623223fe7e8283aff6c687c
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-10 05:26:55 +0100

    Re-structured HTML building in edit.php.

--- code/edit.php
+++ code/edit.php
@@ -1,9 +1,4 @@
 <?php
-echo '<title>Editing "'.$title.'"</title>
-</head>
-<body>
-<p>'.$title.': <a href="plomwiki.php?title='.$title.'">Back to View</a></p>';
-
 # If no page file is found, start with an empty $text.
 if (is_file($page_path)) $text = file_get_contents($page_path); 
 else $text = '';
@@ -14,10 +9,12 @@ $text = str_replace('<', '&lt;',  $text);
 $text = str_replace('>', '&gt;',  $text);
 
 # Final HTML.
-echo 
-'<form method="post" action="plomwiki.php?title='.$title.'&amp;action=write">
-<textarea name="text" rows="10" cols="40">
-'.$text.'
+echo '<title>Editing "'.$title.'"</title>
+</head>
+<body>
+<p>'.$title.': <a href="plomwiki.php?title='.$title.'">Back to View</a></p>
+<form method="post" action="plomwiki.php?title='.$title.'&amp;action=write">
+<textarea name="text" rows="10" cols="40">'.$text.'
 </textarea><br />
 Password: <input type="password" name="password" /><br />
 <input type="submit" value="Update!" />
