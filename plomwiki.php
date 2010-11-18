d32ab657fc71107d1fc6678a58477f761b11842e
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-18 05:16:47 +0100

    Moved all of the markups and the applying of them into its own functions.

--- plomwiki.php
+++ plomwiki.php
@@ -42,15 +42,7 @@ function Action_view()
   # Get $text from its page file.
   if (is_file($page_path)) 
   { $text = file_get_contents($page_path); 
-    $text = EscapeHTML($text);
-    
-    # Line-break and paragraph markup.
-    $text = str_replace("\r\n\r", "\n".'</p>'."\n".'<p>', $text);
-    $text = str_replace("\r", '<br />', $text);
-    
-    # Wiki-internal linking markup [[LikeThis]].
-    $text = preg_replace('/\[\[([A-Za-z0-9]+)\]\]/', 
-                             '<a href="plomwiki.php?title=$1">$1</a>', $text); }
+    $text = Markup($text); }
   
   # If no page file is found, show invitation to create one.
   else $text = 'Page does not exist. <a href="plomwiki.php?title='.$title.
@@ -174,14 +166,29 @@ Finished!
 # Markup functions #
 ####################
 
+function Markup($text)
+# Applying markup functions in a certain order to $text.
+{ $text = EscapeHTML($text);
+  $text = MarkupLinesParagraphs($text);
+  return  MarkupInternalLinks($text); }
+
 function EscapeHTML($text)
 # Replace symbols that might be confused for HTML markup with HTML entities.
 { $text = str_replace('&', '&amp;', $text);
   $text = str_replace('<', '&lt;',  $text); 
   $text = str_replace('>', '&gt;',  $text);
   $text = str_replace('\'', '&apos;',  $text); 
-  $text = str_replace('"', '&quot;',  $text); 
-  return $text; }
+  return  str_replace('"', '&quot;',  $text); }
+
+function MarkupLinesParagraphs($text)
+# Line-break and paragraph markup.
+{ $text = str_replace("\r\n\r", "\n".'</p>'."\n".'<p>', $text);
+  return  str_replace("\r", '<br />', $text); }
+
+function MarkupInternalLinks($text)
+# Wiki-internal linking markup [[LikeThis]].
+{ return preg_replace('/\[\[([A-Za-z0-9]+)\]\]/', 
+                             '<a href="plomwiki.php?title=$1">$1</a>', $text); }
 
 ###################################
 # Database manipulation functions #
