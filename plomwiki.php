e4deae0af452c9cf0e1bccd04affe5426ca1b19e
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-18 04:52:59 +0100

    Re-structured code: Moved the scripts in action/ as functions into plomwiki.php.

--- plomwiki.php
+++ plomwiki.php
@@ -8,12 +8,11 @@ if (!preg_match('/^[a-zA-Z0-9]+$/', $title)) { echo 'Bad page title'; exit(); }
 $pages_dir  = 'pages/';
 $page_path = $pages_dir.$title;
 
-# Find appropriate code for user's '?action='. Assume "view.php" if not found.
-$actions_dir  = 'actions/';
-$fallback = $actions_dir.'view.php';
+# Find appropriate code for user's '?action='. Assume "view" if not found.
+$fallback = 'Action_view';
 $action = $_GET['action'];
-$action_path = ($actions_dir.$action.'.php'); 
-if (!is_file($action_path)) $action_path = $fallback;
+$action_function = 'Action_'.$action;
+if (!function_exists($action_function)) $action_function = $fallback;
 
 # Database manipulation files/dirs. Do urgent work if urgent todo file is found.
 $work_dir = 'work/';
@@ -27,12 +26,14 @@ echo '<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 ';
-require($action_path);
+eval($action_function.'();');
 echo '
 </body>
 </html>';
 
-# Database manipulation functions.
+###################################
+# Database manipulation functions #
+###################################
 
 function WorkToDo($path_todo)
 # Work through todo file. Comment-out finished lines. Delete file when finished.
@@ -91,3 +92,153 @@ function UpdatePage($page_path, $temp_path)
 { if (!is_file($temp_path)) return;
   if (is_file($page_path)) unlink($page_path);
   rename($temp_path, $page_path); }
+
+################
+# Page actions #
+################
+
+function Action_view()
+# Formatted display of a page.
+{ global $page_path, $title;
+  
+  # Get $text from its page file.
+  if (is_file($page_path)) 
+  { $text = file_get_contents($page_path); 
+
+    # Replace symbols that might be confused for HTML markup with HTML entities.
+    $text = str_replace('&', '&amp;', $text);
+    $text = str_replace('<', '&lt;',  $text); 
+    $text = str_replace('>', '&gt;',  $text); 
+    $text = str_replace('\'', '&apos;',  $text); 
+    $text = str_replace('"', '&quot;',  $text); 
+    
+    # Line-break and paragraph markup.
+    $text = str_replace("\r\n\r", "\n".'</p>'."\n".'<p>', $text);
+    $text = str_replace("\r", '<br />', $text);
+    
+    # Wiki-internal linking markup [[LikeThis]].
+    $text = preg_replace('/\[\[([A-Za-z0-9]+)\]\]/', 
+                             '<a href="plomwiki.php?title=$1">$1</a>', $text); }
+  
+  # If no page file is found, show invitation to create one.
+  else $text = 'Page does not exist. <a href="plomwiki.php?title='.$title.
+                                                '&amp;action=edit">Create?</a>';
+  
+  # Final HTML.
+  echo '<title>'.$title.'</title>
+</head>
+<body>
+<p>
+'.$title.': <a href="plomwiki.php?title='.$title.'&amp;action=edit">Edit</a>
+</p>
+<p>
+'.$text.'
+</p>'; }
+
+function Action_edit()
+# Edit form on a page source text. Send results to ?action=write.
+{ global $page_path, $title;
+  
+  # If no page file is found, start with an empty $text.
+  if (is_file($page_path)) $text = file_get_contents($page_path); 
+  else $text = '';
+  
+  # Replace symbols that might be confused for HTML markup with HTML entities.
+  $text = str_replace('&', '&amp;', $text);
+  $text = str_replace('<', '&lt;',  $text); 
+  $text = str_replace('>', '&gt;',  $text);
+  $text = str_replace('\'', '&apos;',  $text); 
+  $text = str_replace('"', '&quot;',  $text); 
+  
+  # Final HTML.
+  echo '<title>Editing "'.$title.'"</title>
+</head>
+<body>
+<p>
+'.$title.': <a href="plomwiki.php?title='.$title.'">Back to View</a>
+</p>
+<form method="post" action="plomwiki.php?title='.$title.'&amp;action=write">
+<textarea name="text" rows="10" cols="40">'.$text.'
+</textarea><br />
+Password: <input type="password" name="password" /><br />
+<input type="submit" value="Update!" />
+</form>'; }
+
+function Action_write()
+# Password-protected writing of page update to work/.
+{ global $page_path, $title, $todo_urgent;
+  
+  # The edited page text submitted by edit.php.
+  $text = $_POST['text'];
+  
+  # Start by checking for edit failure conditions.
+  $html_start = '<title>Trying to edit "'.$title.'"</title>';
+  
+  # Check for failure condition: wrong password.
+  $password_posted = $_POST['password'];
+  $password_expected = substr(file_get_contents('password.txt'), 0, -1);
+  if ($password_posted !== $password_expected)
+    $message = '<strong>Wrong password.</strong>';
+  
+  # Check for failure condition: empty $text.
+  elseif (!$text)
+    $message = 
+'<strong>Empty pages not allowed.</strong><br />
+Replace the page text with "delete" if you want to eradicate the page.';
+  
+  # Successful edit writes to todo_urgent, triggers work on it and a redirect.
+  else
+  { $html_start = 
+                '<meta http-equiv="refresh" content="0; URL=plomwiki.php?title='
+                                                     .$title.'" />'.$html_start;
+    $p_todo = fopen($todo_urgent, 'a+');
+    
+    # "delete" triggers page deletion.
+    if ($text == 'delete')
+    { if (is_file($page_path)) 
+        fwrite($p_todo, 'DeletePage("'.$title.'");'."\n");
+      $message = '<strong>Page "'.$page_path.'" is now non-existant.</strong>';}
+  
+    else
+    { # Undo damage that results from PHP's magical_quotes horrors.
+      if (get_magic_quotes_gpc()) $text = stripslashes($text);
+      
+      # Write $text into a temp file to be given to UpdatePage().
+      $temp_path = NewTempFile($text);
+      fwrite($p_todo, 'UpdatePage("'.$page_path.'", "'.$temp_path.'");'."\n");
+      $message = '<strong>Page "'.$title.'" updated.</strong>'; }
+    
+    fclose($p_todo);
+    WorkToDo($todo_urgent);
+    
+    # Message for very speedy readers or very slow redirects.
+    $message .= '<br />
+If you read this, then your browser failed to redirect you back.'; }
+  
+  # Final HTML.
+  echo $html_start.'
+</head>
+<body>
+<p>
+'.$message.'
+</p>
+<p>
+Return to page "<a href="plomwiki.php?title='.$title.'">'.$title.'</a>".
+</p>'; }
+
+function Action_work()
+# Work through todo list.
+{ global $work_dir;
+
+  $path_todo = $work_dir.'todo';
+  
+  echo '<title>Doing some processing work ...</title>
+</head>
+<body>
+<p>
+Doing some processing work ...
+</p>';
+  WorkToDo($path_todo);
+  echo '<p>
+Finished!
+</p>'; }
