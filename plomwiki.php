807078d784e35ee4785ef080b5c5ca097ca863d5
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-11-21 03:35:04 +0100

    Minor comment addition in Action_work().

--- plomwiki.php
+++ plomwiki.php
@@ -186,9 +186,9 @@ function Action_write()
 function Action_work()
 # Work through todo list.
 { global $work_dir;
-
   $path_todo = $work_dir.'todo';
   
+  # Final HTML
   echo '<title>Doing some processing work ...</title>'."\n".'</head>'."\n".
           '<body>'."\n".'<p>'."\n".'Doing some processing work ...'."\n".'</p>';
   WorkToDo($path_todo);
