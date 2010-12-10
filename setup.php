1bc1625e97d5c1130afc59f2fde1a9f66d776376
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-12-10 23:48:36 +0100

    Repaired HTML generated by setup.php

--- setup.php
+++ setup.php
@@ -13,13 +13,8 @@ if ($fail)   $msg = 'PlomWiki Setup failed. Something is wrong!';
 else       { $msg = 'PlomWiki Setup successful. Reload!'; unlink($setup_file); }
 
 # Final HTML.
-echo $html_start.'PlomWiki Setup</title>
-</head>
-<body>
-
-<h1>PlomWiki Setup</h1>
-
-<p><strong>'; echo $msg;
-echo '</strong></p>'.$html_end;
+echo $html_start.'PlomWiki Setup</title>'."\n".'</head>'."\n".'<body>'."\n\n".
+     '<h1>PlomWiki Setup</h1>'."\n\n".'<p><strong>'.$msg.'</strong></p>'."\n\n".
+                                                       '</body>'."\n".'</html>';
 
 exit();
