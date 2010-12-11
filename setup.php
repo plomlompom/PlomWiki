f263e2bafb61651aa6c9355ab190669743868abd
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-12-11 23:21:35 +0100

    New start page (the intro text from the README, slightly modified.) As this adds pages/diff/, this part of the directory need no more be created by setup.php

--- setup.php
+++ setup.php
@@ -5,7 +5,6 @@ $fail = FALSE;
 $cwd = getcwd();
 if (!mkdir($cwd.'/'.$work_dir))      $fail = TRUE;
 if (!mkdir($cwd.'/'.$work_temp_dir)) $fail = TRUE;
-if (!mkdir($cwd.'/'.$diff_dir))      $fail = TRUE;
 if (!mkdir($cwd.'/'.$del_dir))       $fail = TRUE;
 
 # Answer according to success.
