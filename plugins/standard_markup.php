d7da464c92bad5a80976d3848f14a77e2ded6871
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-12-13 05:03:21 +0100

    Some variable re-naming / re-combination in standard_markup.php

--- plugins/standard_markup.php
+++ plugins/standard_markup.php
@@ -11,12 +11,14 @@ $markup_help = '<h4>PlomWiki markup cheatsheet</h4>'."\n".'<p>In-line:</p>'."\n"
 '<pre>*] list element'."\n".'  *] indented once'."\n".
 '    *] indented twice</pre>';
 
-# Escaping variables. $esc."\n" newlines won't be replaced by "<br />.
-# Bracket lines to exclude from "<p>" paragraphing in $esc_p_on and $esc_p_off.
-$esc = "\r";
-$esc_p_on = $esc.'p_on';
-$esc_p_off = $esc.'p_off';
-$escaped = array();
+# Escaping variables. $markup_esc."\n" newlines won't be replaced by "<br />.
+# Bracket lines to exclude from "<p>" in $markup_esc_p_on and $markup_esc_p_off.
+# User-escaped strings are stored via Escape() / Unescape() in $markup_escaped.
+$markup_esc = "\r";
+$markup_esc_p_on = $markup_esc.'p_on';
+$markup_esc_p_off = $markup_esc.'p_off';
+$markup_esc_user = '['.$markup_esc.']';
+$markup_escaped = array();
 
 ##################
 # In-line markup #
@@ -52,26 +54,29 @@ function MarkupDeleted($text)
 # Multiple-lines markup #
 #########################
 
-function Escape($text)
-# Replace "[\This"\]" with "[\r]", store "This" in array $escaped.
-{ global $escaped;
-  preg_match_all('/\[\\\(.*?)\\\]/s', $text, $escaped);
-  $text = preg_replace('/\[\\\.*?\\\]/s', '['."\r".']', $text);
+function MarkupEscape($text)
+# Replace "[\This"\]" with "[\r]", store "This" in array $markup_escaped.
+{ global $markup_escaped, $markup_esc_user;
+
+  preg_match_all('/\[\\\(.*?)\\\]/s', $text, $markup_escaped);
+  $markup_escaped = $markup_escaped[1];
+  $text           = preg_replace('/\[\\\.*?\\\]/s', $markup_esc_user, $text);
   return $text; }
 
-function Unescape($text)
+function MarkupUnescape($text)
 # Replace all "[\r]" with the string originally escaped.
-{ global $escaped;
-  foreach ($escaped[1] as $string)
-  { $pos = strpos($text, '['."\r".']');
+{ global $markup_escaped, $markup_esc_user;
+
+  foreach ($markup_escaped as $string)
+  { $pos         = strpos($text, $markup_esc_user);
     $text_before = substr($text, 0, $pos);
-    $text_after = substr($text, $pos + 3);
-    $text = $text_before.$string.$text_after; }
+    $text_after  = substr($text, $pos + strlen($markup_esc_user));
+    $text        = $text_before.$string.$text_after; }
   return $text; }
 
 function MarkupLists($text)
 # Lines starting with '*] ' preceded by multiples of double whitespace -> lists.
-{ global $esc, $esc_p_on, $esc_p_off;
+{ global $markup_esc, $markup_esc_p_on, $markup_esc_p_off;
 
   # Add temporary final line to $text for final line-by-line comparision.
   $lines = explode("\n", $text."\n");
@@ -89,7 +94,7 @@ function MarkupLists($text)
       if ($line_start == $li_on)
       { $failed_tries = 0;
         $line_end = substr($line, $ln_li_on);
-        $lines[$n] = $depth.'<li>'.$line_end.'</li>'.$esc; } }
+        $lines[$n] = $depth.'<li>'.$line_end.'</li>'.$markup_esc; } }
     $li_on = '  '.$li_on; 
     $ln_li_on = strlen($li_on); 
     $depth++; }
@@ -105,27 +110,27 @@ function MarkupLists($text)
     if ($depth > $last_depth)
     { $add = '';                                    # $add contains "<ul><li>"
       for ($i = $last_depth + 1; $i < $depth; $i++) # steps for depth
-        $add .= $i.'<ul><li>'.$esc."\n";            # differences beyond 1.
-      $lines[$n] = $add.$depth.'<ul>'.$esc."\n".$lines[$n]; 
+        $add .= $i.'<ul><li>'.$markup_esc."\n";     # differences beyond 1.
+      $lines[$n] = $add.$depth.'<ul>'.$markup_esc."\n".$lines[$n]; 
       if ($last_depth == -1) # If last line be non-list, prepend "<p />" escape.
-        $lines[$n] = $esc_p_on.$lines[$n];
+        $lines[$n] = $markup_esc_p_on.$lines[$n];
       else                   # Else, open last line's "<li />" for "<ul>".
-        $lines[$n - 1] = substr($lines[$n - 1], 0, -6).$esc; }
+        $lines[$n - 1] = substr($lines[$n - 1], 0, -6).$markup_esc; }
 
     # As depth descends, add "</ul>" and "<li>" as needed.
     elseif ($depth < $last_depth)
     { for ($i = $depth; $i < $last_depth; $i++)
-      { if ($i == -1)              # Unescaped "\n" provides first non-list line
-          $add = $esc_p_off."\n";  # with "\n\n" start, allowing "<p>" opening.
+      { if ($i == -1)                    # Unescaped "\n" opens non-list line
+          $add = $markup_esc_p_off."\n"; # with "\n\n" start, allows for "<p>".
         else
-         $add = '</li>'.$esc."\n"; 
+         $add = '</li>'.$markup_esc."\n"; 
         $lines[$n] = 1 + $i.'</ul>'.$add.$lines[$n]; } }
 
     $last_depth = $depth; }
   
   # Transform depth numbers before "<li>", "<ul>" and "</ul>" into whitespace.
   foreach ($lines as $n => $line)
-  { $lines_sub = explode($esc."\n", $line);
+  { $lines_sub = explode($markup_esc."\n", $line);
     foreach ($lines_sub as $n_sub => $line_sub)
     { $match = preg_match('/([0-9]+)(<((li)|(ul)|(\/ul))\>)/',$line_sub,$catch);
       if ($match)
@@ -133,17 +138,17 @@ function MarkupLists($text)
         $replace = str_pad('', $depth*2);
         $lines_sub[$n_sub] = preg_replace('/[0-9]+(?=(<li>)|(<ul>)|(<\/ul>))/', 
                                                        $replace, $line_sub); } }
-    $lines[$n] = implode($esc."\n", $lines_sub); }
+    $lines[$n] = implode($markup_esc."\n", $lines_sub); }
 
   # Implode lines back to $text. Remove temporary final line added earlier.
   $text = implode("\n", $lines); $text = substr($text, 0, -1); return $text; }
 
 function MarkupLinesParagraphs($text)
 # Line-break and paragraph markup.
-{ global $esc, $esc_p_on, $esc_p_off;
+{ global $markup_esc, $markup_esc_p_on, $markup_esc_p_off;
 
-  # Temporarily replace escaped newlines them with $esc.'n'.
-  $text = str_replace($esc."\n",                               $esc.'n', $text);
+  # Temporarily replace escaped newlines them with $markup_esc.'n'.
+  $text = str_replace($markup_esc."\n", $markup_esc.'n', $text);
 
   # Unescaped newlines get transformed into "<br \>" and "<p />".
   $text = str_replace("\n",                                    '<br />', $text); 
@@ -153,13 +158,13 @@ function MarkupLinesParagraphs($text)
   # Assume $text starts, ends as paragraph. If wrong, will be corrected later.
   $text = '<p>'."\n".$text."\n".'</p>';
 
-  # All replacing of "\n" done, it's safe to replace $esc.'n' with it.
-  $text = str_replace($esc.'n',                                    "\n", $text);
+  # All replacing of "\n" done, it's safe to replace $markup_esc.'n' with it.
+  $text = str_replace($markup_esc.'n',                             "\n", $text);
 
-  # Move anything bracketed between $esc_p_on and $esc_p_off out of paragraphs.
-  $text = str_replace('<p>'."\n".$esc_p_on,                          '', $text);
-  $text = str_replace($esc_p_off."\n".'</p>',                        '', $text);
-  $text = str_replace('<br />'."\n".$esc_p_on,         "\n".'</p>'."\n", $text);
-  $text = str_replace($esc_p_off.'<br />',                   "\n".'<p>', $text);
+  # Move anything bracketed between $markup_esc_p_{on,off} out of paragraphs.
+  $text = str_replace('<p>'."\n".$markup_esc_p_on,                   '', $text);
+  $text = str_replace($markup_esc_p_off."\n".'</p>',                 '', $text);
+  $text = str_replace('<br />'."\n".$markup_esc_p_on,  "\n".'</p>'."\n", $text);
+  $text = str_replace($markup_esc_p_off.'<br />',            "\n".'<p>', $text);
   
   return $text; }
