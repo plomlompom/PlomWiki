ed0a6ae56dc695616e1390b61fe8c7c288cc84c8
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-12-18 12:53:19 +0100

    Heavily refactored AutoLink().

--- plugins/AutoLink.php
+++ plugins/AutoLink.php
@@ -21,8 +21,7 @@ function MarkupAutolink($text)
   # Get $links_out from $cur_page_file, turn into regex from their resp. files.
   $links_out = Autolink_GetFromFileLine($cur_page_file, 1, TRUE);
   foreach ($links_out as $pagename)
-  { $linked_page_file = $AutoLink_dir.$pagename;
-    $regex_pagename = Autolink_GetFromFileLine($linked_page_file, 0);
+  { $regex_pagename = AutoLink_RetrieveRegexForTitle($pagename);
     
     # Build autolinks into $text where $avoid applies.
     $avoid = '(?=[^>]*($|<(?!\/(a|script))))';
@@ -32,85 +31,65 @@ function MarkupAutolink($text)
   
   return $text; }
 
-#########################
-# DB building / purging #
-#########################
+####################################
+# DB updating / building / purging #
+####################################
 
 function UpdateAutoLinks($t, $text, $diff)
 # Add to task list $t AutoLink DB update tasks. $text, $diff determine change.
 { global $AutoLink_dir, $nl, $title;
   $cur_page_file = $AutoLink_dir.$title;
+  $all_other_titles = array_diff(GetAllPageTitles(), array($title));
 
   # Silently fail if AutoLink DB directory does not exist.
-  if (!is_dir($AutoLink_dir))
-    return $t;
-
-  # Page deletion severs links between files before deletion of $cur_page_file.
-  if ($text == 'delete')
-  { $links_out = Autolink_GetFromFileLine($cur_page_file, 1, TRUE);
-    foreach ($links_out as $pagename)
-      $t[] = array('AutoLink_RemoveFromLine', $pagename.'_2_'.$title);
-    $links_in = Autolink_GetFromFileLine($cur_page_file, 2, TRUE);
-    foreach ($links_in as $pagename)
-      $t[] = array('AutoLink_RemoveFromLine', $pagename.'_1_'.$title);
-    $t[] = array('unlink', $cur_page_file); }
+  if (!is_dir($AutoLink_dir)) return $t;
 
   # Page creation demands new file, going through all pages for new AutoLinks.
-  elseif (!is_file($cur_page_file))
+  if (!is_file($cur_page_file))
   { $t[] = array('AutoLink_CreateFile', $title);
-    foreach (GetAllPageTitles() as $linkable)
-      if ($linkable != $title)
-      { $t[] = array('AutoLink_TryLinking', $title.'_'.$linkable);  
-        $t[] = array('AutoLink_TryLinking', $linkable.'_'.$title); } }
+    foreach ($all_other_titles as $linkable)
+    { $t[] = array('AutoLink_TryLinking', $title.'_'.$linkable);
+      $t[] = array('AutoLink_TryLinking', $linkable.'_'.$title); } }
 
-  # For mere page change, determine tasks by comparing diff against $links_out.
   else
-  { # Divide $diff into $diff_del / $diff_add: lines deleted / added.
-    $lines = explode($nl, $diff);
-    $diff_del = array();
-    $diff_add = array();
-    foreach ($lines as $line)
-      if     ($line[0] == '<')
-        $diff_del[] = substr($line, 1);
-      elseif ($line[0] == '>')
-        $diff_add[] = substr($line, 1);
+  { $links_out  = Autolink_GetFromFileLine($cur_page_file, 1, TRUE);
 
-    # Compare unlinked titles' regexes against new lines to harvest $links_new.
-    $titles = GetAllPageTitles();
-    $links_new = array();
-    $links_out = Autolink_GetFromFileLine($cur_page_file, 1, TRUE);
-    $yet_unlinked = array_diff($titles, $links_out, array($title));
-    foreach ($yet_unlinked as $pagename)
-    { $linked_page_file = $AutoLink_dir.$pagename;
-      $regex = Autolink_GetFromFileLine($linked_page_file, 0);
-      foreach ($diff_add as $line)
-        if (preg_match('/'.$regex.'/iu', $line))
-        { $links_new[] = $pagename; 
-          break; } }
-    foreach ($links_new as $pagename)
-    { $t[] = array('AutoLink_InsertInLine', $title.'_1_'.$pagename);
-      $t[] = array('AutoLink_InsertInLine', $pagename.'_2_'.$title); }
+    # Page deletion severs links between files before $cur_page_file deletion.
+    if ($text == 'delete')
+    { foreach ($links_out as $pagename)
+        $t[] = array('AutoLink_ChangeLine', $pagename.'_2_out_'.$title);
+      $links_in = Autolink_GetFromFileLine($cur_page_file, 2, TRUE);
+      foreach ($links_in as $pagename)
+        $t[] = array('AutoLink_ChangeLine', $pagename.'_1_out_'.$title);
+      $t[] = array('unlink', $cur_page_file); }
 
-    # Match $links_out's regexes against deleted lines. Reduce $links_maybe_dead
-    # by matching first against new lines, secondly against whole page $text.
-    $links_maybe_dead = array();
-    foreach ($links_out as $pagename)
-    { $linked_page_file = $AutoLink_dir.$pagename;
-      $regex = Autolink_GetFromFileLine($linked_page_file, 0);
-      foreach ($diff_del as $line)
-        if (preg_match('/'.$regex.'/iu', $line))
-          $links_maybe_dead[$pagename] = $regex; }
-    foreach ($links_maybe_dead as $pagename => $regex)
-      foreach ($diff_add as $line)
-        if (preg_match('/'.$regex.'/iu', $line))
-        { unset($links_maybe_dead[$pagename]);
-          break; }
-    foreach ($links_maybe_dead as $pagename => $regex)
-      if (preg_match('/'.$regex.'/iu', $text))
-        unset($links_maybe_dead[$pagename]);
-    foreach ($links_maybe_dead as $pagename)
-    { $t[] = array('AutoLink_RemoveFromLine', $title.'_1_'.$pagename);
-      $t[] = array('AutoLink_RemoveFromLine', $pagename.'_2_'.$title); } }
+    # For mere page change, determine tasks comparing $diff against $links_out.
+    else
+    { # Divide $diff into $diff_del / $diff_add: lines deleted / added.
+      $lines = explode($nl, $diff);
+      $diff_del = array(); $diff_add = array();
+      foreach ($lines as $line)
+      if     ($line[0] == '<') $diff_del[] = substr($line, 1);
+      elseif ($line[0] == '>') $diff_add[] = substr($line, 1);
+  
+      # Compare unlinked titles' regexes against $diff_add: harvest $links_new.
+      $links_new = array();
+      $not_linked = array_diff($all_other_titles, $links_out);
+      foreach (AutoLink_TitlesInLines($not_linked, $diff_add) as $pagename)
+        $links_new[] = $pagename;
+      $t = AutoLink_TasksLinksInOrOut($t, 'in', $title, $links_new);
+ 
+      # Threaten $links_out by matches in $diff_del. Remove threat if regexes
+      # still matched in $diff_add or whole page $text. Else remove link_out.
+      $links_rm = array();
+      foreach (AutoLink_TitlesInLines($links_out, $diff_del) as $pagename)
+        $links_rm[] = $pagename;
+      foreach (AutoLink_TitlesInLines($links_rm, $diff_add) as $pagename)
+        $links_rm = array_diff($links_rm, array($pagename)); 
+      $lines_text = explode($nl, $text);
+      foreach (AutoLink_TitlesInLines($links_rm, $lines_text) as $pagename)
+        $links_rm = array_diff($links_rm, array($pagename));
+      $t = AutoLink_TasksLinksInOrOut($t, 'out', $title, $links_rm); } }
 
   return $t; }
 
@@ -129,7 +108,7 @@ function Action_autolink_start_db()
 
 function PrepareWrite_autolink_start_db()
 # Deliver to Action_write() all information needed for AutoLink DB building.
-{ global $AutoLink_dir, $nl, $root_rel, $pages_dir, $todo_urgent;
+{ global $AutoLink_dir, $nl, $root_rel, $todo_urgent;
 
   # Variables easily produced.
   $x['todo'] = $todo_urgent;
@@ -195,95 +174,100 @@ function PrepareWrite_autolink_destroy_db()
 function AutoLink_CreateFile($title)
 # Start AutoLink file of page $title, empty but for title regex.
 { global $AutoLink_dir, $nl2;
-
-  # $content (at start empty but for first, regex line) shall rest at $path.
   $path    = $AutoLink_dir.$title;
   $content = $title.$nl2;
-  
-  # Put $content into temp file for SafeWrite() to harvest.
-  $path_temp = NewTempFile($content);
-  SafeWrite($path, $path_temp); }
+  AutoLink_SendToSafeWrite($path, $content); }
 
-function AutoLink_TryLinking($titles)
+function AutoLink_TryLinking($input_string)
 # $titles = $title_$linkable. Try auto-linking both pages, write to their files.
 { global $AutoLink_dir, $nl, $pages_dir;
-  list($title, $linkable) = explode('_', $titles);
-  $page_txt = file_get_contents($pages_dir.$title);
 
-  $path_linkable = $AutoLink_dir.$linkable;
-  $regex_linkable = Autolink_GetFromFileLine($path_linkable, 0);
+  # Get $title, $linkable from $titles. (Hack around WriteTasks().)
+  list($title, $linkable) = explode('_', $input_string);
+
+  $page_txt       = file_get_contents($pages_dir.$title);
+  $regex_linkable = AutoLink_RetrieveRegexForTitle($linkable);
   if (preg_match('/'.$regex_linkable.'/iu', $page_txt))
-  { AutoLink_InsertInLine($title.'_1_'.$linkable);
-    AutoLink_InsertInLine($linkable.'_2_'.$title); } }
+  { AutoLink_ChangeLine($title.'_1_in_'.$linkable);
+    AutoLink_ChangeLine($linkable.'_2_in_'.$title); } }
 
-function AutoLink_InsertInLine($string)
-# Add in $title's pagefile on $line_n $insert (all variables found in $string).
+function AutoLink_ChangeLine($input_string)
+# On $title's AutoLink file, on $line_n, move $diff in/out according to $action.
 { global $AutoLink_dir, $nl;
 
-  # Get $title, $line_n, $insert from $string.
-  list($title, $line_n, $insert) = explode('_', $string);
+  # Get variables from exploded input string. (Hack around WriteTasks().)
+  list($title, $line_n, $action, $diff) = explode('_', $input_string);
+  $path = $AutoLink_dir.$title;
 
-  # Get $content from $title's AutoLink file, add $insert.whitespace on $line_n.
-  $path_file      = $AutoLink_dir.$title;
-  $lines          = explode($nl, file_get_contents($path_file));
-  $line           = $lines[$line_n];
-  $line           = rtrim($line);
-  $strings        = explode(' ', $line);
-  $strings[]      = $insert;
-  usort($strings, 'AutoLink_SortByLengthAlphabetCase');
-  $line           = implode(' ', $strings);
-  $lines[$line_n] = rtrim($line).' ';
+  # Do $action with $diff on $title's file $line_n. Re-sort line for "in".
+  $lines          = explode($nl, file_get_contents($path));
+  $strings        = explode(' ', $lines[$line_n]);
+  if     ($action == 'in')
+  { $strings[]    = $diff;
+    usort($strings, 'AutoLink_SortByLengthAlphabetCase'); }
+  elseif ($action == 'out')
+    $strings      = array_diff($strings, array($diff));  
+  $new_line       = implode(' ', $strings);
+  $lines[$line_n] = rtrim($new_line);
   $content        = implode($nl, $lines);
-    
-  # Put $content into temp file for SafeWrite() to harvest.
-  $path_temp= NewTempFile($content);
-  SafeWrite($path_file, $path_temp); }
 
-function AutoLink_RemoveFromLine($string)
-# From $title's pagefile on $line_n $remove (all variables found in $string).
-{ global $AutoLink_dir, $nl;
-
-  # Get $title, $line_n, $remove from $string.
-  list($title, $line_n, $remove) = explode('_', $string);
-
-  # Get file content from $title's AutoLink file, remove $remove on $line_n.
-  $path_file      = $AutoLink_dir.$title;
-  $lines          = explode($nl, file_get_contents($path_file));
-  $line           = rtrim($lines[$line_n]);
-  $elements       = explode(' ', $line);
-  $elements       = array_diff($elements, array($remove));  
-  $line           = implode(' ', $elements).' ';
-  $lines[$line_n] = ltrim($line);
-  $content        = implode($nl, $lines);
-    
-  # Put $content into temp file for SafeWrite() to harvest.
-  $path_temp= NewTempFile($content);
-  SafeWrite($path_file, $path_temp); }
+  AutoLink_SendToSafeWrite($path, $content); }
 
 ##########################
 # Minor helper functions #
 ##########################
 
-function Autolink_GetFromFileLine($path, $line, $return_as_array = FALSE)
-# Return $line of file $path. $return_as_array string separated by ' ' if set.
+function Autolink_GetFromFileLine($path, $line_n, $return_as_array = FALSE)
+# Return $line_n of file $path. $return_as_array string separated by ' ' if set.
+# From empty lines, explode() generates $x = array(''); return array() instead.
 { global $nl;
   $x = explode($nl, file_get_contents($path));
-  $x = $x[$line];
+  $x = $x[$line_n];
   if ($return_as_array)
-  { $x = rtrim($x);
-    $x = explode(' ', $x); 
-    if (!$x[0])
-      return array(); }
+    $x = explode(' ', $x);
+    if ($x == array(''))
+      return array();
   return $x; }
 
 function Autolink_SortByLengthAlphabetCase($a, $b)
 # Try to sort by stringlength, then follow sort() for uppercase vs. lowercase.
 { $strlen_a = strlen($a);
   $strlen_b = strlen($b);
-  if ($strlen_a < $strlen_b) return 1;
+  if     ($strlen_a < $strlen_b) return  1;
   elseif ($strlen_a > $strlen_b) return -1;
 
   $sort = array($a, $b);
   sort($sort);
   if ($sort[0] == $a) return -1;
-  return 1; }
+  else                return  1; }
+
+function AutoLink_RetrieveRegexForTitle($title)
+# Return regex matching $title according to its AutoLink file.
+{ global $AutoLink_dir;
+  $AutoLink_file = $AutoLink_dir.$title;
+  $regex = Autolink_GetFromFileLine($AutoLink_file, 0);
+  return $regex; }
+
+function AutoLink_TitlesInLines($titles, $lines)
+# Return array of all $titles whose AutoLink regex matches $lines.
+{ $titles_new = array();
+  foreach ($titles as $title)
+  { $regex = AutoLink_RetrieveRegexForTitle($title);
+    foreach ($lines as $line)
+      if (preg_match('/'.$regex.'/iu', $line))
+      { $titles_new[] = $title;
+        break; } }
+  return $titles_new; }
+
+function AutoLink_TasksLinksInOrOut($tasks, $dir, $title, $titles)
+# Add $tasks of moving $titles $dir ('in'/'out') of line 1 in $title's AutoLink
+# file and $title $dir ('in'/'out')of line 2 in $titles' AutoLink files.
+{ foreach ($titles as $pagename)
+  { $tasks[] = array('AutoLink_ChangeLine', $title.'_1_'.$dir.'_'.$pagename);
+    $tasks[] = array('AutoLink_ChangeLine', $pagename.'_2_'.$dir.'_'.$title); }
+  return $tasks; }
+
+function AutoLink_SendToSafeWrite($path, $content)
+# Call SafeWrite() not on $content directly, but on newly built temp file of it.
+{ $path_temp= NewTempFile($content);
+  SafeWrite($path, $path_temp); }
