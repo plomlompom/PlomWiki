306e7a3956878320c304f57dad8ceac957fcd32d
Christian Heller / @plomlompom <c.heller@plomlompom.de>
2010-12-23 06:35:52 +0100

    Corrected small bug in GetAllPageTitles().

--- plugins/StandardLib.php
+++ plugins/StandardLib.php
@@ -4,6 +4,7 @@ function GetAllPageTitles()
 # Return an array of all of the PlomWiki page's titles.
 { global $pages_dir, $legal_title; 
   $p_dir = opendir($pages_dir);
+  $titles = array();
   while (FALSE !== ($fn = readdir($p_dir)))
     if (is_file($pages_dir.$fn) and preg_match('/^'.$legal_title.'$/', $fn))
       $titles[] = $fn;
