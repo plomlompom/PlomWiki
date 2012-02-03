84ac17d0ea56620028345e8780a4f960660a1306
Christian Heller <c.heller@plomlompom.de>
2012-02-03 02:00:52 +0100

    Removed plugins not absolutely needed for a minimal PlomWiki. They will have their own repository.

--- plugins/Today.php
+++
@@ -1,7 +0,0 @@
-<?php
-# PlomWiki plugin Today
-# Provides [:today:] markup.
-
-function MarkupToday($text)
-# Translates [:today:] into the current date, formatted as Y-m-d.
-{ return str_replace('[:today:]', date('Y-m-d'), $text); }
